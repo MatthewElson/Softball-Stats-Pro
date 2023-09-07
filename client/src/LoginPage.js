09e6fa64bf5df13541b7b1afdb5321adc6ddd37b
aguerraochoa <aguerraochoa4@gmail.com>
2023-09-07 15:21:58 -0600

    Changed firebase credentials and added useState to login inputs

--- client/src/LoginPage.js
+++ client/src/LoginPage.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const LoginPage = () => {
+
+    const [teamName, setTeamName] = useState("");
+    const [teamPassword, setTeamPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    }
+
     return (
         <section className="login">
             <h1>Softball Stats Pro</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="team-name">Team Name</label>
-                <input id="team-name" type="text" required/>
+                <input id="team-name" type="text" name="teamName" value={teamName} onChange={(e) => setTeamName(e.target.value)} required/>
                 <label htmlFor="team-password">Password</label>
-                <input id="team-password" type="password" required/>
+                <input id="team-password" type="password" name="teamPassword" value={teamPassword} onChange={(e) => setTeamPassword(e.target.value)} required/>
                 <button type="submit">Sign In</button>
             </form>
         </section>
