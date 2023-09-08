44cb3f399c6e24f1ed4a6449b820346e390e6dd0
aguerraochoa <aguerraochoa4@gmail.com>
2023-09-08 15:39:52 -0600

    Stat buttons and useState hooks properly working

--- client/src/NewGame.js
+++ client/src/NewGame.js
@@ -5,26 +5,89 @@ const NewGame = () => {
 
     const [players, setPlayers] = useState([
         "Alonso",
-        "Guerra"
+        "Guerra",
+        "Miguel",
+        "Rogelio"
     ])
 
     const [gameStats, setGameStats] = useState([
+        ["Single", "Strikeout", "Double", "Triple"],
+        ["Single", "Strikeout", "Double", "Triple"],
         ["Single", "Strikeout", "Double", "Triple"],
         ["Single", "Strikeout", "Double", "Triple"]
     ])
 
     const [rbis, setRbis] = useState([
         0,
+        0,
+        0,
+        0
+    ])
+
+    const [sbs, setSbs] = useState([
+        0,
+        0,
+        0,
         0
     ])
 
     const [average, setAverage] = useState([
         [0, 0],
+        [0, 0],
+        [0, 0],
         [0, 0]
     ])
 
-    const goodButtons = ["Single", "Double", "Triple", "Homerun", "Walk", "Stolen Base"];
+    const [selectedPlayerIdx, setSelectedPlayerIdx] = useState(0);
+
+    const goodButtons = ["Single", "Double", "Triple", "Homerun", "Walk"]
     const badButtons = ["Out", "Strikeout"];
+
+    const handleRadioChange = (e) => {
+        setSelectedPlayerIdx(e.target.value);
+    }
+
+    const handleClick = (e) => {
+
+        const stat = e.target.value;
+
+        if (goodButtons.includes(stat) || badButtons.includes(stat)){
+            const copiedStats = [...gameStats];
+            copiedStats[selectedPlayerIdx].push(stat);
+            setGameStats(copiedStats);
+
+            if (stat !== "Walk"){
+                const copiedAverage = [...average];
+                copiedAverage[selectedPlayerIdx][1] += 1;
+    
+                if (goodButtons.includes(stat)) {
+                    copiedAverage[selectedPlayerIdx][0] += 1;
+                }
+    
+                setAverage(copiedAverage);
+            }
+        }
+
+        else if (stat === "RBI+" || (stat === "RBI-" && rbis[selectedPlayerIdx] > 0)){
+            const copiedRBIs = [...rbis];
+            if (stat === "RBI+"){
+                copiedRBIs[selectedPlayerIdx] += 1;
+            } else {
+                copiedRBIs[selectedPlayerIdx] -= 1;
+            }
+            setRbis(copiedRBIs);
+        }
+
+        else if (stat === "SB+" || (stat === "SB-" && sbs[selectedPlayerIdx] > 0)){
+            const copiedSbs = [...sbs];
+            if (stat === "SB+"){
+                copiedSbs[selectedPlayerIdx] += 1;
+            } else {
+                copiedSbs[selectedPlayerIdx] -= 1;
+            }
+            setSbs(copiedSbs);
+        }
+    }
 
 
     return (
@@ -36,29 +99,45 @@ const NewGame = () => {
                     return player.name;
                 }).join(", ")}
             </p>
-            <h2>Offense</h2>
+            <h2>Offense:</h2>
             <section className="offense-boxes">
                 {players.map((player, idx) => (
-                    <div className="box" key={player}>
-                        <div className="upperbox">
-                            <div>{player}</div>
-                            <div>{average[idx][0]}/{average[idx][1]}</div>
-                            <div>{rbis[idx]} RBI's</div>
-                        </div>
-                        <div className="lowerbox">
-                            <div>{gameStats[idx].join(", ")}</div>
-                        </div>
+                    <div className={`box ${player === players[selectedPlayerIdx] ? "selected" : ""}`} key={player}>
+                        <label htmlFor={player}>
+                            <div className="upperbox">
+                                <div className="player-name">{player}</div>
+                                <div className="player-average">{average[idx][0]}/{average[idx][1]}</div>
+                                <div className="player-sbs">{sbs[idx]} SB's</div>
+                                <div className="player-rbis">{rbis[idx]} RBI's</div>
+                            </div>
+                            <div className="lowerbox">
+                                <div>{gameStats[idx].join(", ")}</div>
+                            </div>
+                        </label>
                     </div>
                 ))}
             </section>
             <section className="offense-good-buttons">
                 {goodButtons.map((btn) => (
-                    <button className="good-button" key={btn} value={btn}>{btn}</button>
+                    <button key={btn} value={btn} onClick={handleClick}>{btn}</button>
                 ))}
+            </section>
+            <section className="offense-sbs-buttons">
+                <button key="SB+" value="SB+" onClick={handleClick}>SB+</button>
+                <button key="SB-" value="SB-" onClick={handleClick}>SB-</button>
+            </section>
+            <section className="offense-rbis-buttons">
+                <button value="RBI+" onClick={handleClick}>RBI+</button>
+                <button value="RBI-" onClick={handleClick}>RBI-</button>
             </section>
             <section className="offense-bad-buttons">
                 {badButtons.map((btn) => (
-                    <button key={btn} value={btn}>{btn}</button>
+                    <button key={btn} value={btn} onClick={handleClick}>{btn}</button>
+                ))}
+            </section>
+            <section className="offense-player-selector">
+                {players.map((player, idx) => (
+                    <input key={player} id={player} type="radio" name="selector" value={idx} checked={player === players[selectedPlayerIdx]} onChange={handleRadioChange} />
                 ))}
             </section>
         </section>
