3c80b4cb0c581dca4dc0b1d57579fa33871763f0
UnusualSoul <elsonmatthew@yahoo.ca>
2025-01-04 23:20:03 -0500

    commented out the password page because that portion isn't setup yet and google keeps flagging the site as malicious

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -67,13 +67,13 @@ const LoginPage = () => {
                                     <Form.Control id="team-name" value={teamName} type="text" onChange={(e) => setTeamName(e.target.value)} placeholder='Team Name' maxLength={35} required/>
                                 </InputGroup>
                                 
-                                <Form.Label htmlFor="team-password" visuallyHidden>Password</Form.Label>
+                                {/* <Form.Label htmlFor="team-password" visuallyHidden>Password</Form.Label>
                                 <InputGroup className="mb-3">
                                     <InputGroup.Text>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-lock" viewBox="0 0 16 16"><path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2M5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1"/></svg>
                                     </InputGroup.Text>
                                     <Form.Control id="team-password" value={guessedPassword} type="password" onChange={(e) => setGuessedPassword(e.target.value)} placeholder='Password' maxLength={50} required/>
-                                </InputGroup>
+                                </InputGroup> */}
                                
                                 <Button type="submit" className='maxWidth'>Sign In</Button>
                             </Form.Group>
