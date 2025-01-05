0a2a5b38d219c208ea6d84d1060d432af93991ad
UnusualSoul <elsonmatthew@yahoo.ca>
2025-01-04 23:15:42 -0500

    changed popups from alerts to toasts to be consistent with the rest of the page, added opponent team name and game type fields

--- src/Modules/SubmitStats.js
+++ src/Modules/SubmitStats.js
@@ -1,11 +1,13 @@
-import React  from 'react';
+import React, { useState }  from 'react';
 // import CloseButton from 'react-bootstrap/CloseButton';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import Toast from 'react-bootstrap/Toast';
 import Globals from '../Globals';
 
-const SubmitStats = ({popupToggle, setPopupToggle, setSecret, handleSubmitStatsForm}) => {
+const SubmitStats = ({popupToggle, setPopupToggle, setSecret, setOpponentTeamName, handleSubmitStatsForm, setGameType, saveGameResult}) => {
+    const gameTypes = ["regular", "playoff", "practice"];
     return (
         <div className="modal show" style={{ display: 'block', position: 'initial' }} >
             <Modal show={popupToggle} onHide={() => Globals.toggleCB(setPopupToggle)}>
@@ -15,6 +17,23 @@ const SubmitStats = ({popupToggle, setPopupToggle, setSecret, handleSubmitStatsF
         
                 <Form onSubmit={handleSubmitStatsForm}>
                     <Modal.Body>
+                        <Form.Group className='mb-2' controlId='opponentTeamName'>
+                            <Form.Label>Opponent Team Name</Form.Label>
+                            <Form.Control type='text' onChange={e => setOpponentTeamName(e.target.value)} required />
+                        </Form.Group>
+                        <Form.Group className='mb-2' controlId='gameType'>
+                            <Form.Label>Game Type</Form.Label>
+                            {gameTypes.map((gameType, idx) =>
+                                <Form.Check
+                                    label= {gameType.replace(/(.)/, (_, chr) => chr.toUpperCase())}
+                                    name= {"gameTypeRadioButton"}
+                                    type={'radio'}
+                                    id={gameType + "Game"}
+                                    key={gameType + idx}
+                                    onChange={() => setGameType(gameType)}
+                                />
+                            )}
+                        </Form.Group>
                         <Form.Group className="mb-2" controlId="formPassword">
                             <Form.Label>Team Password</Form.Label>
                             <Form.Control type="password" placeholder="Password" onChange={(e) => setSecret(e.target.value)} required/>
@@ -27,6 +46,11 @@ const SubmitStats = ({popupToggle, setPopupToggle, setSecret, handleSubmitStatsF
                         <Button type="submit" variant="success">Submit Stats</Button>
                     </Modal.Footer>
                 </Form>
+                <div className="toast-container position-fixed bottom-0 end-0 p-3">
+                    <Toast show={saveGameResult.show} onClose={() => Globals.toggleCB(setPopupToggle)} delay={2000} autohide={saveGameResult.success} bg={saveGameResult.success ? 'success' : 'danger'}>
+                        <Toast.Body>{saveGameResult.message}</Toast.Body>
+                    </Toast>
+                </div>
             </Modal>
         </div>
     )
